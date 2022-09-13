07b8c0a08d3cdd796c09f92b0fba234811f95214
Clinton Evans <study@getctrl.co.nz>
2022-09-14 08:58:11 +1200

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Let's pat our animals")
+const myAnimals = ['Tara','Ezran','Soobie','Chester']
+const yourAnimals = ['Tayo','Evie','Sara','CheeseDog']
+
+function patAnimal(petName) {
+  lovedAnimals = 0 // I wake up having patted ZERO animals!! Usually this is a 'count' or 'counter'
+  petName.forEach((pet) => {
+    lovedAnimals++ // I have just patted an animal!! YUS! 
+    if (pet === 'Tara' || 'Ezran') { // The || is an OR statement. Tara OR Ezran
+      console.log(pet + ' ' + 'got gentle pats') // cats get special treatment
+    } else {
+    console.log(pet + ' ' + 'got the pats!') // normal treatment
+    }
+  })
+  console.log('I have given love to ' + ' ' + lovedAnimals + ' ' + 'animals')
+  //console.log(animal + ' ' + 'got the pats!')
+}
+
+patAnimal(myAnimals)
+console.log("")
+patAnimal(yourAnimals)
