c4ed74b4edb88ac75e5f1ba99bfc0fb857144790
Clinton Evans <study@getctrl.co.nz>
2022-09-14 09:00:27 +1200

    <h1> added

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const yourAnimals = ['Tayo','Evie','Sara','CheeseDog']
 
 function patAnimal(petName) {
   lovedAnimals = 0 // I wake up having patted ZERO animals!! Usually this is a 'count' or 'counter'
-  petName.forEach((pet) => {
-    lovedAnimals++ // I have just patted an animal!! YUS! 
-    if (pet === 'Tara' || 'Ezran') { // The || is an OR statement. Tara OR Ezran
-      console.log(pet + ' ' + 'got gentle pats') // cats get special treatment
+  petName.forEach((pet) => {                        // the pet is the individual pet
+    lovedAnimals++                                  // I have just patted an animal!! YUS! 
+    if (pet === 'Tara' || 'Ezran') {                // The || is an OR statement. Tara OR Ezran
+      console.log(pet + ' ' + 'got gentle pats')    // cats get special treatment
     } else {
-    console.log(pet + ' ' + 'got the pats!') // normal treatment
+    console.log(pet + ' ' + 'got the pats!')        // normal treatment
     }
   })
   console.log('I have given love to ' + ' ' + lovedAnimals + ' ' + 'animals')
